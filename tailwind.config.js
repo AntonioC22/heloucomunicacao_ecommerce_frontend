218d6421de438b9d52ec67f5a41390433c75e606
Antonio Sobrinho <antonio.sobrinho@edu.unifil.br>
2024-11-12 09:32:18 -0300

    chore: configura projeto React com Vite e Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#6ee7b7',
+                    dark: '#34d399',
+                },
+                helou: {
+                    green: '#2c4a42',
+                    bg: '#f8fafb',
+                }
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            }
+        },
+    },
+    plugins: [],
+}
