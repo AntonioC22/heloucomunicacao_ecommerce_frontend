669ee5993dfafa15176976799a3cda4bb7372894
Antonio Sobrinho <antonio.sobrinho@edu.unifil.br>
2024-11-12 15:20:00 -0300

    chore: adestrutura base do react para o frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <h1></h1>
+                <p></p>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
