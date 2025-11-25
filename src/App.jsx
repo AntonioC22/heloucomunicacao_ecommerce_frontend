a50439a24d8733245fcb6ded4a1c2f3555312a5f
Antonio Sobrinho <antonio.sobrinho@edu.unifil.br>
2025-11-25 17:06:00 -0300

    feat: integranfo tela de produtos ao roteamento da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import GerenciarProdutos from './pages/GerenciarProdutos';
 
-function App() {
+
+export default function App() {
     return (
         <BrowserRouter>
-            <div>
-                <h1></h1>
-                <p></p>
+            <div className="min-h-screen bg-gray-50">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Navigate to="/produtos" replace />} />
+                    <Route path="/produtos" element={<GerenciarProdutos />} />
+                </Routes>
             </div>
         </BrowserRouter>
     );
 }
-
-export default App;
